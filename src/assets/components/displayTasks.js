e9520ab106ee2a8a1b8bd7680dbea57c39e2c399
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-04-24 15:12:21 -0300

    Created taskFormatter for displayTasks

--- src/assets/components/displayTasks.js
+++ src/assets/components/displayTasks.js
@@ -1,9 +1,43 @@
-export default function displayTasks() {
+export default function displayTasks(tasks) {
   const taskDisplay = document.createElement('div');
   taskDisplay.classList.add('task-display');
   const h3 = document.createElement('h3');
   h3.textContent = 'Tasks';
   taskDisplay.appendChild(h3);
 
+  if (tasks.length === 0) {
+    const p = document.createElement('p');
+    p.textContent = 'No tasks to display';
+    taskDisplay.appendChild(p);
+  } else {
+    tasks.forEach((task) => {
+      taskDisplay.appendChild(taskFormatter(task));
+    });
+  }
+
   return taskDisplay;
 }
+
+const taskFormatter = (task) => {
+  const taskDiv = document.createElement('div');
+  taskDiv.classList.add('task');
+  const title = document.createElement('h4');
+  title.textContent = task.title;
+  taskDiv.appendChild(title);
+  const description = document.createElement('p');
+  description.textContent = task.description;
+  taskDiv.appendChild(description);
+  const dueDate = document.createElement('p');
+  dueDate.textContent = `Due date: ${task.dueDate}`;
+  taskDiv.appendChild(dueDate);
+  const priority = document.createElement('p');
+  priority.textContent = `Priority: ${task.priority}`;
+  taskDiv.appendChild(priority);
+  const project = document.createElement('p');
+  project.textContent = `Project: ${task.project}`;
+  taskDiv.appendChild(project);
+  const completed = document.createElement('p');
+  completed.textContent = `Completed: ${task.completed}`;
+  taskDiv.appendChild(completed);
+  return taskDiv;
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import displayTasks from './assets/components/displayTasks.js';
 import '../dist/assets/styles/style.css';
 
+const tasks = [
+  {
+    title: 'Task 1',
+    description: 'Task 1 description',
+    dueDate: '2021-12-31',
+    priority: 'High',
+    project: null,
+    completed: false,
+  },
+  {
+    title: 'Task 2',
+    description: 'Task 2 description',
+    dueDate: '2021-12-31',
+    priority: 'Medium',
+    project: null,
+    completed: false,
+  },
+];
+
 let content = document.getElementById('main-content');
-content.appendChild(displayTasks());
+content.appendChild(displayTasks(tasks));
