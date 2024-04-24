09d180f0658a22d679faa70eec81090c4151fca0
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-04-24 15:36:38 -0300

    Inserted button and fixed .main-content overflow

---
+++ src/assets/components/createTask.js
@@ -0,0 +1,3 @@
+export default function createTask() {
+  console.log('button clicked');
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import displayTasks from './assets/components/displayTasks.js';
+import createTask from './assets/components/createTask.js';
 import '../dist/assets/styles/style.css';
 
 const tasks = [
@@ -18,7 +19,34 @@ const tasks = [
     project: null,
     completed: false,
   },
+  {
+    title: 'Task 3',
+    description: 'Task 3 description',
+    dueDate: '2021-12-31',
+    priority: 'Low',
+    project: null,
+    completed: false,
+  },
+  {
+    title: 'Task 4',
+    description: 'Task 4 description',
+    dueDate: '2021-12-31',
+    priority: 'High',
+    project: null,
+    completed: false,
+  },
 ];
 
 let content = document.getElementById('main-content');
+
+let addNewTaskDiv = document.createElement('div');
+addNewTaskDiv.classList.add('add-new-task');
+let addNewTaskButton = document.createElement('button');
+addNewTaskButton.textContent = 'Add New Task';
+addNewTaskDiv.appendChild(addNewTaskButton);
+addNewTaskButton.addEventListener('click', () => {
+  createTask();
+});
+
+content.appendChild(addNewTaskDiv);
 content.appendChild(displayTasks(tasks));
