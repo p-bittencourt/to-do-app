3c46166461c9b2884b502c3a316121458be02327
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-04-26 15:40:04 -0300

    Added background image

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const loader = require("css-loader");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loader = require('css-loader');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      title: "To Do App",
-      favicon: "./src/assets/badge-check_7653142.png",
+      template: './src/index.html',
+      title: 'To Do App',
+      favicon: './src/assets/badge-check_7653142.png',
     }),
   ],
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: "file-loader",
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
 };
