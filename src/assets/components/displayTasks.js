0d311a36f6971eaddcc001b85e5d2c792a744079
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-05-01 18:43:40 -0300

    Improved delete button

--- src/assets/components/displayTasks.js
+++ src/assets/components/displayTasks.js
@@ -80,8 +80,15 @@ const taskCard = (task) => {
 
   // Delete button
   const deleteButton = document.createElement('button');
-  deleteButton.classList.add('btn', 'btn-danger', 'mx-1');
-  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('btn', 'm-1');
+  deleteButton.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <polyline points="3 6 5 6 21 6"></polyline>
+  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m6 0V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2"></path>
+  <line x1="10" y1="11" x2="10" y2="17"></line>
+  <line x1="14" y1="11" x2="14" y2="17"></line>
+  </svg>
+  `;
   deleteButton.addEventListener('click', () => {
     // Call a function to handle the delete action
     const confirmDelete = confirm('Are you sure you want to delete this task?');
