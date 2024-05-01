b7c9f129a4eafef3f489ced79cdefd357e3afdc8
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-05-01 18:46:22 -0300

    Added editButton icon

--- src/assets/components/displayTasks.js
+++ src/assets/components/displayTasks.js
@@ -101,8 +101,13 @@ const taskCard = (task) => {
 
   // Edit button
   const editButton = document.createElement('button');
-  editButton.classList.add('btn', 'btn-light', 'mx-1');
-  editButton.textContent = 'Edit';
+  editButton.classList.add('btn', 'm-1');
+  editButton.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M12 20h9"></path>
+    <path d="M16.5 3.5a2.828 2.828 0 0 1 4 4L12 16l-4 1 1-4 8.5-8.5z"></path>
+  </svg>
+  `;
   editButton.addEventListener('click', () => {
     // Call a function to handle the edit action
     editTask(task);
